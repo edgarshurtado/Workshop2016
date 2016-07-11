d1fbaf86e769aad9c76e570f52a1c12931e02338
Edgar S. Hurtado <edsanhu@gmail.com>
2016-07-11 19:27:42 +0200

    Create module

---
+++ NodeJSimaginaformacion/tema02/ejer01/controllers/Data.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Edgar S. Hurtado on 11/07/16.
+ */
+
+var nombres = ["Mario", "Patricia", "Manolo", "Santi", "David", "Oscar", "Luis", "Saul", "Autor"];
+
+function getNames(){
+    return nombres;
+}
+
+module.exports = {
+    getNames: getNames
+}
