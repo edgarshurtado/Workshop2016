694bc08a9852009d3f154b0ba9198e8289161fe9
Edgar S. Hurtado <edsanhu@gmail.com>
2016-07-11 19:46:28 +0200

    Add Twig template + variables to the '/' route

--- NodeJSimaginaformacion/tema02/ejer01/routes/index.js
+++ NodeJSimaginaformacion/tema02/ejer01/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var nombres = require('../controllers/Data.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hellow Twig', nombres: nombres.getNames() });
 });
 
 module.exports = router;
