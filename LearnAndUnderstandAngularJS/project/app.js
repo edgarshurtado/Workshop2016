4448b235c02e3af6268637711e70e1506f3f5e19
Edgar S. Hurtado <edsanhu@gmail.com>
2016-08-09 19:29:57 +0200

    Create logic SPA aplication. Logic routing WIP

---
+++ LearnAndUnderstandAngularJS/project/app.js
@@ -0,0 +1,31 @@
+var firstAngularApp = angular.module("firstAngularApp", ["ngRoute", "ngResource"]);
+
+firstAngularApp.config(function($routeProvider){
+    $routeProvider
+        .when("/", {
+            templateUrl: "pages/index.html",
+            controller: "indexController"
+        })
+        .when("/forecast", {
+            templateUrl: "pages/forecast.html",
+            controller: "forecastController"
+        });
+});
+
+firstAngularApp.controller("indexController", ["$scope", "dataService", function($scope, dataService){
+    $scope.city = dataService.city;
+
+    $scope.$watch("city", function(){
+        dataService.name = $scope.name
+    })
+}]);
+
+firstAngularApp.controller("forecastController", ["$scope", function($scope){
+    
+}]);
+
+
+// ------------- Services -----------------
+firstAngularApp.service("dataService", function(){
+    this.city = "Gandia"
+});
