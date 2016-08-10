c6a16cbacc24cfb9f91fcaf0ba3698d97673480f
Edgar S. Hurtado <edsanhu@gmail.com>
2016-08-10 18:45:00 +0200

    Implement API call. WIP

--- LearnAndUnderstandAngularJS/project/app.js
+++ LearnAndUnderstandAngularJS/project/app.js
@@ -20,12 +20,24 @@ firstAngularApp.controller("indexController", ["$scope", "dataService", function
     })
 }]);
 
-firstAngularApp.controller("forecastController", ["$scope", "dataService",function($scope, dataService){
+firstAngularApp.controller("forecastController", ["$scope", "dataService", "$resource" ,function($scope, dataService, $resource){
     $scope.city = dataService.city;
+
+    $scope.weatherAPI = $resource(
+        "http://api.openweathermap.org/data/2.5/forecast/daily",
+        // This 2 objects with their values avoid crossorigin issues
+        {callback: "JSON_CALLBACK"},
+        {get: {method: "JSONP"}}
+    );
+
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 2, appid: "9102a05baa45e83f500ef056fa532322"});
+
+
+    console.log($scope)
 }]);
 
 
 // ------------- Services -----------------
 firstAngularApp.service("dataService", function(){
-    this.city = "Gandia"
+    this.city = "Gandia, ES"
 });
