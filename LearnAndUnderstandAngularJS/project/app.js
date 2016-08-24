0003a9f009a283e3d0416eea06485022e99921dd
Edgar S. Hurtado <edsanhu@gmail.com>
2016-08-24 19:38:13 +0200

    Refactor api call to a service

--- LearnAndUnderstandAngularJS/project/app.js
+++ LearnAndUnderstandAngularJS/project/app.js
@@ -6,6 +6,25 @@ firstAngularApp.service("dataService", function(){
     this.city = "Gandia, ES"
 });
 
+firstAngularApp.service("weatherService", ["$resource", function($resource){
+    this.getWeather = function(city, days){
+
+        var weatherAPI = $resource(
+            "http://api.openweathermap.org/data/2.5/forecast/daily",
+            // This 2 objects with their values avoid crossorigin issues
+            {callback: "JSON_CALLBACK"},
+            {get: {method: "JSONP"}}
+        );
+
+        // Get call to the openWeather API
+        return weatherAPI.get({
+            q: city,
+            cnt: days,
+            appid: "9102a05baa45e83f500ef056fa532322"
+        });
+    }
+}]);
+
 // --------------- Directives --------------
 firstAngularApp.directive("weatherResult", function(){
     return {
